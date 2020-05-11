d37a2bc16047fe474229824c223122bcdbfd63d5
Jordan Hayes <hayesja99@gmail.com>
2020-05-11 17:04:36 -0400

    Initial Commit

---
+++ RoseEats/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RoseEats
+//
+//  Created by CSSE Department on 5/11/20.
+//  Copyright © 2020 CSSE Department. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RoseEats/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RoseEats
+//
+//  Created by CSSE Department on 5/11/20.
+//  Copyright © 2020 CSSE Department. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
