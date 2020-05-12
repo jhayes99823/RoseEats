ab334619e9d136243afb4d008be2d784ad8b18b3
Jordan Hayes <hayesja99@gmail.com>
2020-05-12 04:38:23 -0400

    made custom table working on custom cells

--- RoseEats/MenuPageContentViewController.swift
+++ RoseEats/MenuPageContentViewController.swift
@@ -8,16 +8,44 @@
 
 import UIKit
 
-class MenuPageContentViewController: UIViewController {
+class MenuTableCell: UITableViewCell {
+    
+    @IBOutlet weak var menuItemImageView: UIImageView!
+    @IBOutlet weak var menuItemLabel: UILabel!
+}
+
+class MenuPageContentViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var titleLabel: UILabel!
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+    
     var pageIndex: Int = 0
     var strTitle: String!
     
+    var menuPageTableCellID = "MenuItemCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.register(MenuTableCell.self, forCellReuseIdentifier: menuPageTableCellID)
+        tableView.delegate = self
+        tableView.dataSource = self
         titleLabel.text = strTitle
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: menuPageTableCellID) as! MenuTableCell
+        
+        
+        cell.menuItemLabel?.text = self.animals[indexPath.row]
+//        cell.menuItemImageView?.image = UIImage(named: "burger-plain")
+                
+        return cell
+    }
+    
 }
