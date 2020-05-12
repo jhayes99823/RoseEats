29abe7f810b878b64746e57d89b6ad2ee58edb9e
Jordan Hayes <hayesja99@gmail.com>
2020-05-12 19:50:40 -0400

    get restaurant info from database

--- RoseEats/MenuPageContentViewController.swift
+++ RoseEats/MenuPageContentViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class MenuTableCell: UITableViewCell {
     
@@ -15,24 +16,53 @@ class MenuTableCell: UITableViewCell {
 }
 
 class MenuPageContentViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var menuDetailpageSegue = "ShowMenuItemDetailView"
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     
     let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+    var restaurants = [Restaurant]()
     
-    var pageIndex: Int = 0
+    var restaurantRef: CollectionReference!
+    var restaurantListener: ListenerRegistration!
+    
+    var pageIndex: Int!
     var strTitle: String!
     
     var menuPageTableCellID = "MenuItemCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.register(MenuTableCell.self, forCellReuseIdentifier: menuPageTableCellID)
+        
+        restaurantRef = Firestore.firestore().collection("Restaurant")
+        
         tableView.delegate = self
         tableView.dataSource = self
         titleLabel.text = strTitle
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        restaurantListener = restaurantRef.order(by: "Name").addSnapshotListener {
+                   (querySnapShot, error) in if let querySnapShot = querySnapShot {
+                       self.restaurants.removeAll()
+                       querySnapShot.documents.forEach { (docSnapShot) in
+                           self.restaurants.append(Restaurant(documentSnapShot: docSnapShot))
+                       }
+                       self.tableView.reloadData()
+                   }
+                   else {
+                       print("Error getting movie quotes \(error!)")
+                       return
+                   }
+               }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+           super.viewWillDisappear(animated)
+           restaurantListener.remove()
+       }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.animals.count
@@ -41,11 +71,17 @@ class MenuPageContentViewController: UIViewController, UITableViewDelegate, UITa
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: menuPageTableCellID) as! MenuTableCell
         
-        
         cell.menuItemLabel?.text = self.animals[indexPath.row]
-//        cell.menuItemImageView?.image = UIImage(named: "burger-plain")
+        cell.menuItemImageView?.image = UIImage(named: "burger-plain.jpg")
                 
         return cell
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == menuDetailpageSegue {
+            if tableView.indexPathForSelectedRow != nil {
+                (segue.destination as! MenuItemDetailViewController).restaurant = restaurants[self.pageIndex]
+            }
+        }
+    }
 }
