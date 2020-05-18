bd8054d45b3c4665580cf09afad1276892c74d5f
Abizer Naseem <naseema@rose-hulman.edu>
2020-05-18 11:51:30 -0400

    Added RoseFire

--- RoseEats/LoginPageViewController.swift
+++ RoseEats/LoginPageViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 import Firebase
 import GoogleSignIn
+import Rosefire
 
 class LoginPageViewController: UIViewController {
     var showAppSegueID = "LoginPageSegue"
+    let REGISTRY_TOKEN = "aa4cf90e-b9bd-40bc-93dd-ff7f08f8bd19"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,4 +28,22 @@ class LoginPageViewController: UIViewController {
                self.performSegue(withIdentifier: self.showAppSegueID, sender: self)
            }
        }
+    
+    @IBAction func pressedRoseFireLogin(_ sender: Any) {
+        Rosefire.sharedDelegate().uiDelegate = self
+        Rosefire.sharedDelegate().signIn(registryToken: REGISTRY_TOKEN) { (err, result) in
+          if let err = err {
+            print("Rosefire sign in error! \(err)")
+            return
+          }
+          
+          Auth.auth().signIn(withCustomToken: result!.token) { (authResult, error) in
+            if let error = error {
+              print("Firebase sign in error! \(error)")
+              return
+            }
+            self.performSegue(withIdentifier: self.showAppSegueID, sender: self)
+          }
+        }
+    }
 }
