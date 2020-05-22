80d6ee641eebab1181b304449cedaef8b05a4490
Jordan Hayes <hayesja99@gmail.com>
2020-05-22 03:45:20 -0400

    fix some small merge conflicts that remained

--- RoseEats/Models/Order.swift
+++ RoseEats/Models/Order.swift
@@ -24,16 +24,13 @@ class Order {
     var User: String
     var Restaurant: String
     var id: String
-<<<<<<< HEAD
-=======
-    
+
     init(){
         self.Restaurant = ""
         self.User = ""
         self.Items = [OrderItem]()
         self.id = ""
     }
->>>>>>> back-to-start
     
     init(Restaurant: String, User: String, Items: [OrderItem]) {
         self.Restaurant = Restaurant
